3c4fc6f0d51a1ee13ff995e999c481b47370ac0a
Duvier Reinosa <55174272+Duvier-Reinosa@users.noreply.github.com>
2020-04-06 01:36:13 -0500

    Aquí ya tengo una maqueta del diseño

---
+++ calculadora.js
@@ -0,0 +1,11 @@
+var ingresadoSinProceso = document.getElementById("input")
+var ingresado = parseInt(ingresadoSinProceso.value);
+var resultado = document.getElementById("resultado");
+var b = document.getElementById("igual");
+
+// 
+//   function mostrarResultado() {
+//     console.log(ingresado.value);
+//   }
+//
+// b.addEventListener("click", mostrarResultado);
